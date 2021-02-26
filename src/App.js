1ddccb8c1a434810e8a3418721babc1f23ddbaac
Mohit Kumar <mohitpalk2728@gmail.com>
2021-02-26 11:37:24 +0530

    udate

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/header";
 import Body from "./components/body";
+import Footer from "./components/footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Body />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const footer = () => {
+  return (
+    <>
+      <div className="container-fluid fotter-container">
+        <div className="row">
+          <div className="col-12 fotter">
+            <p> Made By Mohit Kumar ❤️ in India.</p>
+            <h5>Github: github.com/mohit27kokanda</h5>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default footer;
